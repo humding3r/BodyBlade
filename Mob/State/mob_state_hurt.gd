626de39b0057c79b9d3a92d5c0387418a8751395
humding3r <jetgeronimo@gmail.com>
2024-03-25 23:21:11 -0500

    Remove unused mob code

--- Mob/State/mob_state_hurt.gd
+++ Mob/State/mob_state_hurt.gd
@@ -24,11 +24,6 @@ func physics_process(delta):
 		change_state("fall")
 
 func calculate_knockback() -> Vector2:
-	#if attacker_area.global_position.x > object.global_position.x:
-		#return Vector2(-250, -base_up_knockback)
-	#if attacker_area.global_position.x < object.global_position.x:
-		#return Vector2(250, -base_up_knockback)
-	print(Vector2.RIGHT.rotated(attacker.global_rotation))
 	if attacker_area.is_in_group("Projectile"):
 		return Vector2(0.0, -base_up_knockback) + (attacker.linear_velocity / 10) \
 			* attacker_knockback + Vector2(RandomNumberGenerator.new().randf_range(-10.0, 10.0), RandomNumberGenerator.new().randf_range(-10.0, 10.0))
