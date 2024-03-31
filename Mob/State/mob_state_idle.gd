89b672a8e065f48b7740a0e3c395513214fc14bf
humding3r <jetgeronimo@gmail.com>
2024-03-31 02:56:51 -0500

    Add mob detection area

--- Mob/State/mob_state_idle.gd
+++ Mob/State/mob_state_idle.gd
@@ -1,8 +1,13 @@
 extends MobState
 
+
+
 func physics_process(delta):
 	apply_gravity(delta)
 	if not object.is_on_floor():
 		if object.velocity.y > 0:
 			change_state("fall")
 	object.velocity.x = int(lerp(object.velocity.x, 0.0, 10 * delta))
+
+func process(delta):
+	pass
